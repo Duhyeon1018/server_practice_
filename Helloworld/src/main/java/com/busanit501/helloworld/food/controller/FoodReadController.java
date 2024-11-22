cffd9e592e7ae95c252065c2d4d54246d762300f
Duhyeon Kang <rkdengus1234@naver.com>
2024-11-22 16:14:52 +0900

    241122 7교시

--- Helloworld/src/main/java/com/busanit501/helloworld/food/controller/FoodReadController.java
+++ Helloworld/src/main/java/com/busanit501/helloworld/food/controller/FoodReadController.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "FoodReadController",urlPatterns = "/food/read")
+@WebServlet(name = "FoodReadController", urlPatterns = "/food/read")
 public class FoodReadController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -20,10 +20,10 @@ public class FoodReadController extends HttpServlet {
         //클릭한 게시글 번호를 가져와야함
         Long tno = Long.parseLong(request.getParameter("tno"));
         //서비스에서 하나의 todo 더미데이터를 조회 후에
-        TodoDTO todoDTO =TodoService.INSTANCE.getOne(5L);
+        TodoDTO todoDTO = TodoService.INSTANCE.getOne(5L);
 
         //화면에 전달하기
-        request.setAttribute("dto",todoDTO);
+        request.setAttribute("dto", todoDTO);
         request.getRequestDispatcher("/WEB-INF/todo/todoRead.jsp")
                 .forward(request, response);
 
